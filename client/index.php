57ad01f889943e26f437f5ccfcdbb2ef214f7691
tera76 <andrea.matera@gmail.com>
2022-07-03 16:50:39 +0200

    client index

--- client/index.php
+++ client/index.php
@@ -6,7 +6,10 @@ echo '<a href="../GoJS/samples/stateChart.html">gojs stateChart</a></br>';
 echo '<a href="../GoJS/samples/kanban.html">gojs kanban</a></br>';
 echo '<a href="../GoJS/samples/flowchart.html">gojs flowchart</a></br>';
 echo '<a href="goldPower.php">goldPower</a></br>';
-echo '<a href="cheDannoStasera.php">cheDannoStasera.php</a></br>';
+
+echo '<br><strong>tv </strong><br><br>';
+// echo '<a href="cheDannoStasera.php">cheDannoStasera.php</a></br>';
+echo '<a href="cheDannoStasera_images.php">cheDannoStasera!</a></br>';
 echo '<a href="cheSuccede.php">cheSuccede.php</a></br>';
 // echo '<a href="../GoJS/samples/network.html">gojs network</a></br>';
 
