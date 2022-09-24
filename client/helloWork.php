c34107d609c454f0af56e4edf6487f36f87e40a7
tera <andrea.matera@gmail.com>
2022-09-24 23:39:27 +0200

    tv add remove in table

--- client/helloWork.php
+++ client/helloWork.php
@@ -353,7 +353,7 @@ The kg Gold is at <a href='$metal_price_current_kg_link'><b>$metal_price_current
 
 <h2>Che Danno Stasera?</h2>
 Simulation:
-<br><a href="cheDannoStasera_images.php">Visit /sibilla/client/cheDannoStasera.php!</a>
+<br><a href="cheDannoStasera.php">Visit /sibilla/client/cheDannoStasera.php!</a>
 
 
 <h2>Che Succede?</h2>
